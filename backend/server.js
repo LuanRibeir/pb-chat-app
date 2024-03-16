83ea45f0fb7cbece466f9138c28d841432f91c2f
Luan Ribeiro <luaneribeiroalmeida@gmail.com>
2024-03-16 19:35:03 -0300

    Add users route

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
+import cookieParser from "cookie-parser";
+
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
@@ -9,6 +12,13 @@ connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // To parse JSON data in the request body
+app.use(express.urlencoded({ extended: true })); // To parse form data in the request body
+app.use(cookieParser()); // To get cookie in the request and set in the response
+
+// Routes
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () =>
   console.log(`Server started at http//localhost:${PORT}`)
