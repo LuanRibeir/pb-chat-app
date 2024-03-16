c1ededfb450508960ce058700b03ae8ce0c6cb0f
Luan Ribeiro <luaneribeiroalmeida@gmail.com>
2024-03-16 17:52:45 -0300

    Add user model

---
+++ backend/models/userModel.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 8,
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    About: {
+      type: String,
+      default: "",
+    },
+    friends: {
+      type: [String],
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
