c506cc2926d38d3abab672144a5bbdc903988cba
Luan Ribeiro <luaneribeiroalmeida@gmail.com>
2024-03-17 11:13:09 -0300

    Initial Frontend Boilerplate for Chakra-UI

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@chakra-ui/button";
+import { Container } from "@chakra-ui/react";
+import { Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <Container maxW="container.lg" centerContent>
+      <Routes></Routes>
+    </Container>
+  );
+}
+
+export default App;
