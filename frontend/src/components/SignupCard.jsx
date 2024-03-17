dd53f88fc97f63dc70792aebdb9e06d8ff0f4803
Luan Ribeiro <luaneribeiroalmeida@gmail.com>
2024-03-17 12:54:12 -0300

    Add Sigup Card

---
+++ frontend/src/components/SignupCard.jsx
@@ -0,0 +1,100 @@
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+  Link,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
+import { useSetRecoilState } from "recoil";
+import authScreenAtom from "../atoms/authAtom";
+
+export default function SignupCard() {
+  const [showPassword, setShowPassword] = useState(false);
+  const setAuthScreen = useSetRecoilState(authScreenAtom);
+
+  return (
+    <Flex align={"center"} justify={"center"}>
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+        <Stack align={"center"}>
+          <Heading fontSize={"4xl"} textAlign={"center"}>
+            Criar Conta
+          </Heading>
+        </Stack>
+        <Box
+          rounded={"lg"}
+          bg={useColorModeValue("white", "gray.dark")}
+          boxShadow={"lg"}
+          p={8}
+          w={{ base: "full", sm: "400px" }}
+        >
+          <Stack spacing={4}>
+            <FormControl>
+              <Box>
+                <FormControl isRequired>
+                  <FormLabel>Nome</FormLabel>
+                  <Input type="text" />
+                </FormControl>
+              </Box>
+            </FormControl>
+            <FormControl isRequired>
+              <FormLabel>Email</FormLabel>
+              <Input type="email" />
+            </FormControl>
+            <FormControl isRequired>
+              <FormLabel>Senha</FormLabel>
+              <InputGroup>
+                <Input type={showPassword ? "text" : "password"} />
+                <InputRightElement h={"full"}>
+                  <Button
+                    variant={"ghost"}
+                    onClick={() =>
+                      setShowPassword((showPassword) => !showPassword)
+                    }
+                  >
+                    {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+            </FormControl>
+            <Stack spacing={10} pt={2}>
+              <Button
+                loadingText="Submitting"
+                size="lg"
+                bg={useColorModeValue("gray.600", "gray.700")}
+                color={"white"}
+                _hover={{
+                  bg: useColorModeValue("gray.700", "gray.800"),
+                }}
+              >
+                Cadastre-se
+              </Button>
+            </Stack>
+            <Stack pt={6}>
+              <Text align={"center"}>
+                Já tem uma conta?{" "}
+                <Link
+                  color={"blue.400"}
+                  onClick={() => {
+                    setAuthScreen("login");
+                  }}
+                >
+                  Login
+                </Link>
+              </Text>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
+}
