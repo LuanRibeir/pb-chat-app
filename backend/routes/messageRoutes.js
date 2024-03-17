dda2a4a47f1720393cbb0906dd7a77507e75ca2d
Luan Ribeiro <luaneribeiroalmeida@gmail.com>
2024-03-16 23:53:01 -0300

    Add get all conversation route

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -5,10 +5,13 @@ import authRoute from "../middlewares/authRoute.js";
 import {
   message_send,
   message_getMessages,
+  message_getConversation,
 } from "../controllers/messageController.js";
 
 const router = express.Router();
 
+// Get all the conversation from a user
+router.get("/conversations", authRoute, message_getConversation);
 // Get all messages from two users
 router.get("/:receiverId", authRoute, message_getMessages);
 // Send message
