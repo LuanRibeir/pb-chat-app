ceae1e67e12f524c3022952c104735a4d06bc529
Luan Ribeiro <luaneribeiroalmeida@gmail.com>
2024-03-17 15:12:30 -0300

    Add route rules on app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,25 @@
 import { Container } from "@chakra-ui/react";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import AuthPage from "./pages/AuthPage";
+import { useRecoilValue } from "recoil";
+import userAtom from "./atoms/userAtom";
+import HomePage from "./pages/HomePage";
+import LogoutButton from "./components/LogoutButton";
 
 function App() {
+  const user = useRecoilValue(userAtom);
+
   return (
     <Container maxW="container.lg">
       <Routes>
-        <Route path="/auth" element={<AuthPage />} />
+        <Route
+          path="/"
+          element={user ? <HomePage /> : <Navigate to="/auth" />}
+        />
+        <Route path="/auth" element={!user ? <AuthPage /> : <HomePage />} />
       </Routes>
+
+      {user && <LogoutButton />}
     </Container>
   );
 }
