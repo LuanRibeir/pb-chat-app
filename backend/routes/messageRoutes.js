53de31c9e32e6745459541477b4042aad5388ed6
Luan Ribeiro <luaneribeiroalmeida@gmail.com>
2024-03-16 23:51:37 -0300

    Add get all messages from two users route

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -2,10 +2,16 @@ import express from "express";
 import authRoute from "../middlewares/authRoute.js";
 
 // Require controller modules
-import { message_send } from "../controllers/messageController.js";
+import {
+  message_send,
+  message_getMessages,
+} from "../controllers/messageController.js";
 
 const router = express.Router();
 
+// Get all messages from two users
+router.get("/:receiverId", authRoute, message_getMessages);
+// Send message
 router.post("/", authRoute, message_send);
 
 export default router;
