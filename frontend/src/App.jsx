d398ba763e028e9fdcef796fb0a42fee1df15b82
Luan Ribeiro <luaneribeiroalmeida@gmail.com>
2024-03-17 12:54:38 -0300

    Set route path for auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
-import { Button } from "@chakra-ui/button";
 import { Container } from "@chakra-ui/react";
-import { Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import AuthPage from "./pages/AuthPage";
 
 function App() {
   return (
-    <Container maxW="container.lg" centerContent>
-      <Routes></Routes>
+    <Container maxW="container.lg">
+      <Routes>
+        <Route path="/auth" element={<AuthPage />} />
+      </Routes>
     </Container>
   );
 }
