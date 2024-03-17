4f1c3ff94778e4d0675395a8b677b61b78dfb8ef
Luan Ribeiro <luaneribeiroalmeida@gmail.com>
2024-03-16 23:14:28 -0300

    Add message routes

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import connectDB from "./db/connectDB.js";
 import cookieParser from "cookie-parser";
 
 import userRoutes from "./routes/userRoutes.js";
+import messageRoutes from "./routes/messageRoutes.js";
 
 dotenv.config();
 
@@ -13,12 +14,14 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Middlewares
 app.use(express.json()); // To parse JSON data in the request body
 app.use(express.urlencoded({ extended: true })); // To parse form data in the request body
 app.use(cookieParser()); // To get cookie in the request and set in the response
 
 // Routes
 app.use("/api/users", userRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () =>
   console.log(`Server started at http//localhost:${PORT}`)
