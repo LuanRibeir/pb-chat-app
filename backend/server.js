297dd325d328a40c9afb76f2673843e8a2f90e9a
Luan Ribeiro <luaneribeiroalmeida@gmail.com>
2024-03-25 19:56:43 -0300

    Add cloudinary api to server

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
 import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
 
 import userRoutes from "./routes/userRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
@@ -13,6 +14,13 @@ connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Cloudinary API
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 // Middlewares
 app.use(express.json()); // To parse JSON data in the request body
