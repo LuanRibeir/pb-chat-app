1ccc276c3d216be6c34b63f870c3ad20d524ad0e
Luan Ribeiro <luaneribeiroalmeida@gmail.com>
2024-03-25 20:00:33 -0300

    Add header and update profile routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,16 +5,23 @@ import { useRecoilValue } from "recoil";
 import userAtom from "./atoms/userAtom";
 import HomePage from "./pages/HomePage";
 import LogoutButton from "./components/LogoutButton";
+import Header from "./components/Header";
+import UpdateProfilePage from "./pages/UpdateProfilePage";
 
 function App() {
   const user = useRecoilValue(userAtom);
 
   return (
     <Container maxW="container.lg">
+      <Header />
       <Routes>
         <Route
           path="/"
           element={user ? <HomePage /> : <Navigate to="/auth" />}
+        />
+        <Route
+          path="/update"
+          element={user ? <UpdateProfilePage /> : <Navigate to="/auth" />}
         />
         <Route path="/auth" element={!user ? <AuthPage /> : <HomePage />} />
       </Routes>
