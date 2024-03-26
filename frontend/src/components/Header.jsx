3774cb80de7d5d949582bda8c42b3d9e307c5d85
Luan Ribeiro <luaneribeiroalmeida@gmail.com>
2024-03-26 13:33:28 -0300

    Change the header to show only to registered users

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,17 +1,50 @@
-import { Flex, Image, useColorMode } from "@chakra-ui/react";
+import { Button, Container, Flex, Image, useColorMode } from "@chakra-ui/react";
+import userAtom from "../atoms/userAtom";
+import { Link as RouterLink } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import { FiLogOut, FiSettings } from "react-icons/fi";
+import useLogout from "../hooks/useLogout";
 
 const Header = () => {
   const { colorMode, toggleColorMode } = useColorMode();
+  const user = useRecoilValue(userAtom);
+  const logout = useLogout();
+
   return (
-    <Flex justifyContent={"center"} mt={6} mb={12}>
-      <Image
-        cursor={"pointer"}
-        alt="logo"
-        w={10}
-        src={colorMode === "dark" ? "/light-logo.svg" : "/dark-logo.svg"}
-        onClick={toggleColorMode}
-      />
-    </Flex>
+    <Container mt={6} mb={12}>
+      {!user && (
+        <Flex justifyContent={"center"}>
+          <Image
+            cursor={"pointer"}
+            alt="logo"
+            w={12}
+            src={colorMode === "dark" ? "/light-logo.svg" : "/dark-logo.svg"}
+            onClick={toggleColorMode}
+          />
+        </Flex>
+      )}
+
+      {user && (
+        <Flex justifyContent={"space-between"} gap={4}>
+          <Image
+            cursor={"pointer"}
+            alt="logo"
+            w={12}
+            src={colorMode === "dark" ? "/light-logo.svg" : "/dark-logo.svg"}
+            onClick={toggleColorMode}
+          />
+
+          <Flex gap={30}>
+            <Button size={"sm"} as={RouterLink} to={"/update"}>
+              <FiSettings size={20} />
+            </Button>
+            <Button size={"sm"} onClick={logout}>
+              <FiLogOut size={20} />
+            </Button>
+          </Flex>
+        </Flex>
+      )}
+    </Container>
   );
 };
 
