fdc7b0887f1bc3cbe549bdff439f038b8b7c6387
LuanRibeir <luaneribeiroalmeida@gmail.com>
2024-03-30 19:06:27 -0300

    Add Chat Page

---
+++ frontend/src/pages/ChatPage.jsx
@@ -0,0 +1,140 @@
+import { SearchIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Button,
+  Flex,
+  Input,
+  Skeleton,
+  SkeletonCircle,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import Conversation from "../components/Conversation";
+import { GiConversation } from "react-icons/gi";
+import MessageContainer from "../components/MessageContainer";
+import { useEffect, useState } from "react";
+import useShowToast from "../hooks/useShowToast";
+import { useRecoilState } from "recoil";
+import {
+  chosenConversationAtom,
+  conversationsAtom,
+} from "../atoms/conversationsAtom";
+
+const ChatPage = () => {
+  const showToast = useShowToast();
+  const [loadingConversations, setLoadingConversations] = useState(true);
+  const [conversations, setConversations] = useRecoilState(conversationsAtom);
+  const [chosenConversation, setChosenConversation] = useRecoilState(
+    chosenConversationAtom
+  );
+
+  useEffect(() => {
+    const getConversations = async () => {
+      try {
+        const res = await fetch("/api/messages/conversations");
+        const data = await res.json();
+        if (data.error) {
+          showToast("Erro", data.message, "error");
+        }
+        setConversations(data);
+      } catch (error) {
+        showToast("Erro", error.message, "error");
+      } finally {
+        setLoadingConversations(false);
+      }
+    };
+    getConversations();
+  }, [showToast, setConversations]);
+
+  return (
+    <Box
+      position={"absolute"}
+      left={"50%"}
+      transform={"translateX(-50%)"}
+      w={{ lg: "800px", md: "80%", base: "100%" }}
+    >
+      <Flex
+        gap={4}
+        flexDirection={{
+          base: "column",
+          md: "row",
+        }}
+        maxW={{ sm: "400px", md: "full" }}
+        mx={"auto"}
+      >
+        <Flex
+          flex={30}
+          gap={2}
+          flexDirection={"column"}
+          maxW={{
+            sm: "250px",
+            md: "full",
+          }}
+          mx={"auto"}
+        >
+          <Text
+            fontWeight={800}
+            color={useColorModeValue("gray.600", "gray.400")}
+          >
+            Sua Conversa
+          </Text>
+
+          <form>
+            <Flex alignItems={"center"} gap={2}>
+              <Input placeholder="Procurar Usuário" />
+              <Button size={"sm"}>
+                <SearchIcon />
+              </Button>
+            </Flex>
+          </form>
+
+          {loadingConversations &&
+            [0, 1, 2, 3, 4].map((_, i) => (
+              <Flex
+                key={i}
+                gap={4}
+                alignItems={"center"}
+                p={"1"}
+                borderRadius={"md"}
+              >
+                <Box>
+                  <SkeletonCircle size={"10"} />
+                </Box>
+                <Flex w={"full"} flexDirection={"column"} gap={3}>
+                  <Skeleton h={"10px"} w={"80px"} />
+                  <Skeleton h={"8px"} w={"90%"} />
+                </Flex>
+              </Flex>
+            ))}
+
+          {!loadingConversations &&
+            conversations.map((conversation) => (
+              <Conversation
+                key={conversation._id}
+                conversation={conversation}
+              />
+            ))}
+        </Flex>
+
+        {!chosenConversation._id && (
+          <Flex
+            flex={70}
+            borderRadius={"md"}
+            p={2}
+            flexDir={"column"}
+            alignItems={"center"}
+            justifyContent={"center"}
+            height={"400px"}
+          >
+            <GiConversation size={100} />
+            <Text fontSize={20}>Selecione um conversa</Text>
+          </Flex>
+        )}
+
+        {chosenConversation._id && <MessageContainer />}
+      </Flex>
+    </Box>
+  );
+};
+
+export default ChatPage;
