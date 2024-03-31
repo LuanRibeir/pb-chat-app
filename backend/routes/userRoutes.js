a9007c8cb165ae16f9669f4dfc5cb3a880a373c2
LuanRibeir <luaneribeiroalmeida@gmail.com>
2024-03-30 22:06:29 -0300

    Add add friend route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -8,6 +8,7 @@ import {
   user_logout,
   user_update,
   user_profile,
+  user_addFriend,
 } from "../controllers/userController.js";
 
 const router = express.Router();
@@ -17,5 +18,6 @@ router.post("/login", user_login);
 router.post("/logout", user_logout);
 router.put("/update/:id", authRoute, user_update);
 router.get("/profile/:email", user_profile);
+router.post("/addFriend/:id", authRoute, user_addFriend);
 
 export default router;
