edd0c0abed163928516884e2643c0868ee6931f6
LuanRibeir <luaneribeiroalmeida@gmail.com>
2024-03-30 22:06:20 -0300

    Implemented add friend on user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -139,4 +139,44 @@ const user_profile = asyncHandler(async (req, res, next) => {
 
   res.status(200).json(user);
 });
-export { user_signup, user_login, user_logout, user_profile, user_update };
+
+const user_addFriend = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+  const friendFound = await User.findById(id);
+  // Find user after the auth by the middleware
+  const currentUser = await User.findById(req.user._id);
+
+  // For the check to work we need to convert the object into a string
+  if (id == req.user._id.toString()) {
+    return res
+      .status(400)
+      .json({ message: "Você não pode adicionar você mesmo" });
+  }
+
+  if (!friendFound || !currentUser) {
+    return res.status(400).json({ message: "Usúario não encontrado." });
+  }
+
+  const isFriend = currentUser.friends.includes(id);
+  if (isFriend) {
+    // Remove friend
+    // Modify currentUser friends, modify friends from friendFound
+    await User.findByIdAndUpdate(req.user._id, { $pull: { friends: id } });
+    await User.findByIdAndUpdate(id, { $pull: { friends: req.user._id } });
+    res.status(200).json({ message: "Amigo removido com sucesso." });
+  } else {
+    // Add Friend
+    await User.findByIdAndUpdate(req.user._id, { $push: { friends: id } });
+    await User.findByIdAndUpdate(id, { $push: { friends: req.user._id } });
+    res.status(200).json({ message: "Amigo adicionado com sucesso." });
+  }
+});
+
+export {
+  user_signup,
+  user_login,
+  user_logout,
+  user_addFriend,
+  user_profile,
+  user_update,
+};
