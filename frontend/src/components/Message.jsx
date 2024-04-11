912e1beb64d4bd4c2583e16d6d9347a6a36bc7fb
LuanRibeir <luaneribeiroalmeida@gmail.com>
2024-04-11 20:52:37 -0300

    Fix message text showing when not existing

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -11,17 +11,21 @@ const Message = ({ ownMessage, message }) => {
     <>
       {ownMessage ? (
         <Flex gap={2} alignSelf={"flex-end"}>
-          <Text maxWidth={"350px"} bg={"blue.400"} p={1} borderRadius={"md"}>
-            {message.text}
-          </Text>
+          {message.text && (
+            <Text maxWidth={"350px"} bg={"blue.400"} p={1} borderRadius={"md"}>
+              {message.text}
+            </Text>
+          )}
           <Avatar src={user.profilePicture} w={7} h={7} />
         </Flex>
       ) : (
         <Flex gap={2}>
           <Avatar src={chosenConversation.profilePicture} w={7} h={7} />
-          <Text maxWidth={"350px"} bg={"gray.600"} p={1} borderRadius={"md"}>
-            {message.text}
-          </Text>
+          {message.text && (
+            <Text maxWidth={"350px"} bg={"gray.600"} p={1} borderRadius={"md"}>
+              {message.text}
+            </Text>
+          )}
         </Flex>
       )}
     </>
