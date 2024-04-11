48c61f8604c1db0b9617d0a9c73d9583a4f7703f
LuanRibeir <luaneribeiroalmeida@gmail.com>
2024-04-11 17:20:40 -0300

    Implement socket.io socket server

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,13 @@ import { v2 as cloudinary } from "cloudinary";
 import userRoutes from "./routes/userRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
 
+import { app, server } from "./socket/socket.js";
+
 dotenv.config();
 
 // Connect to MongoDB
 connectDB();
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Cloudinary API
@@ -31,6 +32,6 @@ app.use(cookieParser()); // To get cookie in the request and set in the response
 app.use("/api/users", userRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () =>
+server.listen(PORT, () =>
   console.log(`Server started at http//localhost:${PORT}`)
 );
